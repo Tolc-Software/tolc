84f73a14a9ed58bcbb6433aad7c8c019c299ff1a
srydell <simonwrydell@gmail.com>
2020-06-02 18:08:33 +0200

    Further streamline commandline parsing

--- src/CommandLine/buildConfig.cpp
+++ src/CommandLine/buildConfig.cpp
@@ -1,40 +1,14 @@
 #include "CommandLine/buildConfig.hpp"
 #include "CommandLine/Config.hpp"
+#include "CommandLine/LanguageConfig/python.hpp"
 #include <Frontend/Python/frontend.hpp>
 #include <Parser/Parse.hpp>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <lyra/lyra.hpp>
 #include <optional>
-#include <sstream>
 
 namespace CommandLine {
-
-namespace {
-struct PythonConfig {
-	static char const* name() {
-		return "python";
-	}
-	std::filesystem::path inputFile;
-	std::filesystem::path outputDirectory;
-
-	PythonConfig(lyra::cli_parser& cli) {
-		cli.add_argument(lyra::opt(inputFile, "input")
-		                     .name("-i")
-		                     .name("--input")
-		                     .required()
-		                     .help("The interface file to be parsed."));
-		cli.add_argument(
-		    lyra::opt(outputDirectory, "output")
-		        .name("-o")
-		        .name("--output")
-		        .required()
-		        .help(
-		            "The output directory where the bindings will be stored."));
-	}
-};
-}    // namespace
 
 std::optional<CommandLine::Config> buildConfig(int argc, const char** argv) {
 	auto cli = lyra::cli_parser();
@@ -42,7 +16,7 @@ std::optional<CommandLine::Config> buildConfig(int argc, const char** argv) {
 	bool show_help = false;
 	cli.add_argument(lyra::help(show_help));
 	cli.add_argument(lyra::arg(language, "language")
-	                     .choices(PythonConfig::name())
+	                     .choices(LanguageConfig::Python::name())
 	                     .required()
 	                     .help("language to perform."));
 	auto result = cli.parse({argc, argv});
@@ -51,16 +25,13 @@ std::optional<CommandLine::Config> buildConfig(int argc, const char** argv) {
 		return {};
 	}
 
-	if (language == PythonConfig::name()) {
-		PythonConfig p(cli);
+	if (language == LanguageConfig::Python::name()) {
+		LanguageConfig::Python p(cli);
 		result = cli.parse({argc, argv});
 		if (result) {
 			CommandLine::Config c;
 			std::cout << "Got input: " << p.inputFile << '\n';
 			std::cout << "Got output: " << p.outputDirectory << '\n';
-			std::string fileBasename;
-			std::stringstream ss(p.inputFile);
-			std::getline(ss, fileBasename, '.');
 			c.m_language = language;
 			c.m_inputFile = p.inputFile;
 			c.m_outputDirectory = p.outputDirectory;
--- src/tolc.main.cpp
+++ src/tolc.main.cpp
@@ -5,26 +5,23 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <lyra/lyra.hpp>
-#include <sstream>
+
+namespace {}
 
 int main(int argc, const char** argv) {
 	if (auto maybeConfig = CommandLine::buildConfig(argc, argv)) {
 		auto config = maybeConfig.value();
 		if (auto ns = Parser::parseFile(config.m_inputFile)) {
-			std::string fileBasename;
-			std::stringstream ss(config.m_inputFile);
-			std::getline(ss, fileBasename, '.');
-			ns->m_name = fileBasename;
+			ns->m_name = config.m_inputFile.stem();
 			auto [file, bindings] = Frontend::Python::createModule(ns.value());
 			std::filesystem::create_directory(config.m_outputDirectory);
 			std::cout << "Bindings are in file: "
 			          << config.m_outputDirectory / file << '\n';
 			std::ofstream out {config.m_outputDirectory / file};
 			out << bindings;
-			return 0;
+			return EXIT_SUCCESS;
 		}
 	}
 
-	return 1;
+	return EXIT_FAILURE;
 }
