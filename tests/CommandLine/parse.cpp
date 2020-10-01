cbe1d188d710638f672f556da5a819729d387b78
srydell <simonwrydell@gmail.com>
2020-10-01 19:27:10 +0200

    Add the first test

---
+++ tests/CommandLine/parse.cpp
@@ -0,0 +1,10 @@
+#include "CommandLine/parse.hpp"
+#include <boost/ut.hpp>
+#include <filesystem>
+#include <optional>
+#include <string>
+
+// std::optional<CommandLine::CLIResult> parse(int argc, const char** argv);
+int main() {
+	boost::ut::expect(true);
+}
