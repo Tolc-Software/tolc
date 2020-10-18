15c57f453097a75c4c8157fd4038e4bd6a483325
srydell <simonwrydell@gmail.com>
2020-10-18 16:30:31 +0200

    From start to finish

--- src/TolcInternal/run.cpp
+++ src/TolcInternal/run.cpp
@@ -1,12 +1,30 @@
 #include "TolcInternal/run.hpp"
 #include "CommandLine/parse.hpp"
 #include "TolcInternal/buildConfig.hpp"
-#include <CLI/CLI.hpp>
 #include <Frontend/Python/frontend.hpp>
+#include <IR/ir.hpp>
 #include <Parser/Parse.hpp>
-#include <iostream>
+#include <filesystem>
+#include <fstream>
 
 namespace TolcInternal {
+
+/**
+  * Check the language passed in an call the appropriate frontend on the input namespace
+  */
+std::pair<std::filesystem::path, std::string>
+callFrontend(TolcInternal::Config::Language language,
+             IR::Namespace const& globalNamespace) {
+	std::filesystem::path file;
+	std::string content;
+	switch (language) {
+		case TolcInternal::Config::Language::Python:
+			std::tie(file, content) =
+			    Frontend::Python::createModule(globalNamespace);
+			break;
+	}
+	return {file, content};
+}
 
 int run(int argc, const char** argv) {
 	if (auto maybeResult = CommandLine::parse(argc, argv)) {
@@ -16,17 +34,26 @@ int run(int argc, const char** argv) {
 			return 0;
 		}
 
+		// Validate user input
 		if (auto maybeConfig = buildConfig(cliResult)) {
 			auto config = maybeConfig.value();
-			std::cout << "Got config with input: " << config.inputFile << '\n';
+
+			// Try to parse it
 			if (auto maybeGlobalNamespace =
 			        Parser::parseFile(config.inputFile, config.parserConfig)) {
 				auto globalNamespace = maybeGlobalNamespace.value();
-				globalNamespace.m_name = "MyModule";
-				auto [path, content] =
-				    Frontend::Python::createModule(globalNamespace);
-				std::cout << "File: " << path << '\n';
-				std::cout << content << '\n';
+				globalNamespace.m_name = config.moduleName;
+
+				auto [file, content] =
+				    callFrontend(config.language, globalNamespace);
+
+				std::filesystem::create_directories(config.outputDirectory);
+				std::ofstream outFile;
+				outFile.open(config.outputDirectory / file);
+				if (outFile.is_open()) {
+					outFile << content;
+				}
+
 				return 0;
 			}
 		}
