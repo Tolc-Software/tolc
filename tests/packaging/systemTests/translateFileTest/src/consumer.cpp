00c59a71070882b1b312a0567de75498b27c5328
srydell <simonwrydell@gmail.com>
2021-01-24 18:19:39 +0100

    Move the tests in translateFileTest to the consumer executable runtime

--- tests/packaging/systemTests/translateFileTest/src/consumer.cpp
+++ tests/packaging/systemTests/translateFileTest/src/consumer.cpp
@@ -1,9 +1,13 @@
 #include "consumer.hpp"
-
-int consume(int i) {
-	return i;
-}
+#include "check_variables.hpp"
+#include <filesystem>
+#include <iostream>
 
 int main() {
+	if (!std::filesystem::exists(getExpectedOutFile())) {
+		std::cerr << "Output file does not exist: " << getExpectedOutFile()
+		          << '\n';
+		return 1;
+	}
 	return 0;
 }
